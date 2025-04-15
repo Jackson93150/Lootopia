1a1f8292fdf5ac30e6e27abb1ce56062617e0da8
tanguy.jouvenceau <tanguy.jouvenceau@adventori.com>
2025-04-15 12:01:35 +0200

    refacto

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,31 +1,21 @@
-import React from 'react';
+import type { ReactNode } from "react";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div
-            className="flex flex-col justify-center items-center h-screen bg-cover bg-center bg-[url('/images/backgrounds/backgroundAuth.png')]">
-
-            <img
-                src="/images/coffre3.png"
-                alt="coffre"
-                className="fixed bottom-0 z-50 w-48 left-1/2 translate-x-[30%]"
-            />
-            <div
-                className="relative bg-yellow-orange p-8 rounded-xl shadow-[0_8px_16px_rgba(0,0,0,0.3)] max-w-[400px] w-full border-l-[2px] border-r-[2px] border-b-[2px] border-[#FCEEA9]">
-                <img
-                    src="/images/lootopiaLogo.png"
-                    alt="Overlay"
-                    className="absolute top-[-100px] left-1/2 -translate-x-1/2 z-50"
-                />
-                <img
-                    src="/images/Shop-Card-Gems-Title.png"
-                    alt="decor"
-                    className="absolute top-0 left-0 w-full scale-x-[1.02]"
-                />
-                <div className="mt-28">
-                    {children}
-                </div>
-            </div>
-        </div>
-    );
+export default function AuthLayout({ children }: { children: ReactNode }) {
+	return (
+		<div className="flex flex-col justify-center items-center h-screen bg-cover bg-center bg-[url('/images/backgrounds/backgroundAuth.png')]">
+			<img
+				src="/images/coffre3.png"
+				alt="coffre"
+				className="fixed bottom-0 z-50 w-48 left-1/2 translate-x-[30%]"
+			/>
+			<div className="relative max-w-[400px] w-full bg-[url('/images/Shop-Card-Gems-Title.png')] bg-no-repeat bg-contain bg-center pb-8">
+				<img
+					src="/images/lootopiaLogo.png"
+					alt="Overlay"
+					className="fixed top-[0px] left-1/2 -translate-x-1/2 z-50 w-60"
+				/>
+				<div className="mt-28 p-8">{children}</div>
+			</div>
+		</div>
+	);
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -9,12 +10,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode;
+	children: ReactNode;
 }>) {
 	return (
 		<html lang="fr">
-		<body className="overflow-hidden">
-		{children}</body>
+			<body className="overflow-hidden">{children}</body>
 		</html>
 	);
 }
