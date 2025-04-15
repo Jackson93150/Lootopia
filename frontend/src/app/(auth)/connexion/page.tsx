669637d5ae8c11c90fe28c38647bb2eba6db9be4
Jackson Anthonipillai <jacksonanthonipillai@MacBook-Pro-de-Jackson-2.local>
2025-04-15 15:33:26 +0200

    format fix

--- frontend/src/app/(auth)/connexion/page.tsx
+++ frontend/src/app/(auth)/connexion/page.tsx
@@ -1,6 +1,5 @@
-import AuthLayout from "@/app/(auth)/layout";
-import ConnexionForm from "@/components/forms/connexionForm";
+import ConnexionForm from "@/components/forms/connexionForm"
 
 export default function ConnexionPage() {
-	return <ConnexionForm />;
+  return <ConnexionForm />
 }
--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,21 +1,17 @@
-import type { ReactNode } from "react";
+import type { ReactNode } from "react"
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-	return (
-		<div className="flex flex-col justify-center items-center h-screen bg-cover bg-center bg-[url('/images/backgrounds/backgroundAuth.png')]">
-			<img
-				src="/images/coffre3.png"
-				alt="coffre"
-				className="fixed bottom-0 z-50 w-48 left-1/2 translate-x-[30%]"
-			/>
-			<div className="relative max-w-[400px] w-full bg-[url('/images/Shop-Card-Gems-Title.png')] bg-no-repeat bg-contain bg-center pb-8">
-				<img
-					src="/images/lootopiaLogo.png"
-					alt="Overlay"
-					className="fixed top-[0px] left-1/2 -translate-x-1/2 z-50 w-60"
-				/>
-				<div className="mt-28 p-8">{children}</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-cover bg-center bg-[url('/images/backgrounds/backgroundAuth.png')]">
+      <img src="/images/coffre3.png" alt="coffre" className="fixed bottom-0 z-50 w-48 left-1/2 translate-x-[30%]" />
+      <div className="relative max-w-[400px] w-full bg-[url('/images/Shop-Card-Gems-Title.png')] bg-no-repeat bg-contain bg-center pb-8">
+        <img
+          src="/images/lootopiaLogo.png"
+          alt="Overlay"
+          className="fixed top-[0px] left-1/2 -translate-x-1/2 z-50 w-60"
+        />
+        <div className="mt-28 p-8">{children}</div>
+      </div>
+    </div>
+  )
 }
