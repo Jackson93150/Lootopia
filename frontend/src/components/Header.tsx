f509c3345bb970a1b6c3b85c8fd9bbc0546bd7b0
tanguy.jouvenceau <tanguy.jouvenceau@adventori.com>
2025-05-19 15:26:35 +0200

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,43 +1,52 @@
 "use client";
 
 import Image from "next/image";
+import {RankDisplay} from "@/components/ui/RankDisplay";
+import { useMe } from "@/app/hook/useMe";
+import { useRouter } from "next/navigation"
 
 export default function Header() {
+    const { user, loading } = useMe()
+    const router = useRouter()
     return (
         <header
             className="fixed w-full bg-cover top-0 left-0 z-50 h-[90px] bg-center bg-[url('/images/backgrounds/Group.png')]"
             style={{backgroundSize: "100% 100%"}}
         >
-            <div className="flex items-center justify-between px-6 ">
-                {/* Gauche : Logo + navigation */}
+            <div className="flex items-center h-[90px] justify-between px-6 ">
                 <div className="flex items-center gap-6">
                     <Image
                         src="/images/lootopiaLogo.png"
                         alt="Logo Lootopia"
-                        width={100}
-                        height={40}
-                        className="h-auto"
+                        width={140}
+                        height={70}
+                        className="h-auto -mb-14 w-[100px] md:w-[140px]"
                     />
 
-                    {/* Navigation */}
                     <nav className="flex items-center gap-4">
                         <button
-                            className="text-2xl text-white drop-shadow-lg tracking-wider font-extrabold font-lilita hover:underline transition">
+                            onClick={() => router.push("/hotel-de-vente")}
+                            className="text-xl md:text-2xl text-white drop-shadow-lg tracking-wider font-extrabold font-lilita hover:scale-105 transition">
                             BOUTIQUE
                         </button>
                         <button
-                            className="text-2xl text-white drop-shadow-lg tracking-wider font-extrabold font-lilita hover:underline transition">
+                            onClick={() => router.push("/profile")}
+                            className="text-xl md:text-2xl text-white drop-shadow-lg tracking-wider font-extrabold font-lilita hover:scale-105 transition">
                             INVENTAIRE
                         </button>
                         <button
-                            className="text-2xl text-white drop-shadow-lg tracking-wider font-extrabold font-lilita hover:underline transition">
+                            className="text-xl md:text-2xl text-white drop-shadow-lg tracking-wider font-extrabold font-lilita hover:scale-105 transition">
                             CHASSE
                         </button>
                     </nav>
                 </div>
 
-                {/* Droite : Profil */}
-                <p className="text-black font-fredoka font-medium">Profil</p>
+                {!loading && (
+                <div onClick={() => router.push("/profile")}
+                     className="scale-[0.75] origin-right cursor-pointer">
+                    <RankDisplay user={user} />
+                </div>
+                    )}
             </div>
         </header>
 
