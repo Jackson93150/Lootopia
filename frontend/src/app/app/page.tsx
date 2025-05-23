0ea517ebd0d1b6f6c7a50c7c52fbf4ebe7c28860
tanguy.jouvenceau <tanguy.jouvenceau@adventori.com>
2025-05-23 13:25:54 +0200

    homepage

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -6,6 +6,7 @@ import { Canvas } from "@react-three/fiber"
 import { Autofocus, BrightnessContrast, EffectComposer } from "@react-three/postprocessing"
 import { motion, useAnimation } from "framer-motion"
 import { useEffect, useRef } from "react"
+import Image from "next/image"
 
 export default function AppPage() {
   const earthRef = useRef(null)
@@ -46,30 +47,73 @@ export default function AppPage() {
   }, [controls, canvasControls])
 
   return (
-    <div className="bg-gradient-to-t from-[#a2cbf4] via-[#69BDF2] to-[#0150fa] relative w-screen h-screen flex justify-center">
-      {/* <div className="absolute top-50 left-6 z-10">
-        <StylishButton variant="bag" onClick={() => console.log("clicked")} />
-      </div> */}
-      <motion.img
-        src="/images/title.png"
-        alt="title"
-        className="absolute w-[40vw] will-change-transform"
-        initial={{ scale: 0 }}
-        animate={controls}
-      />
+      <div
+          className="bg-gradient-to-t from-[#a2cbf4] via-[#69BDF2] to-[#0150fa] relative w-screen h-screen flex justify-center">
 
-      <motion.div initial={{ opacity: 0 }} animate={canvasControls} className="w-full h-full pointer-events-none">
-        <Canvas gl={{ powerPreference: "high-performance" }} camera={{ position: [0, 0, 5], fov: 40 }}>
-          <ambientLight intensity={0.4} />
-          <directionalLight position={[50, 50, 15]} intensity={6.5} />
-          <Earth earthRef={earthRef} />
-          <OrbitControls enableRotate={false} enableZoom={false} enablePan={false} />
-          <EffectComposer multisampling={0} enableNormalPass>
-            <BrightnessContrast contrast={-0.15} brightness={-0.01} />
-            <Autofocus />
-          </EffectComposer>
-        </Canvas>
-      </motion.div>
-    </div>
+
+        <motion.img
+            src="/images/title.png"
+            alt="title"
+            className="absolute w-[40vw] will-change-transform"
+            initial={{scale: 0}}
+            animate={controls}
+        />
+
+        <div className="absolute bottom-[10vh] z-10 flex gap-20 justify-center items-center pointer-events-auto">
+          <motion.button
+              className="duration-300 hover:scale-105 cursor-pointer"
+              onClick={() => console.log("organiser")}
+              animate={{scale: [1, 1.03, 1]}}
+              transition={{
+                duration: 4,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "easeInOut",
+              }}
+          >
+            <Image
+                src="/images/boutonChasse.png"
+                alt="Organiser une chasse"
+                width={450}
+                height={90}
+                priority
+            />
+          </motion.button>
+
+          <motion.button
+              className="duration-300 hover:scale-105 cursor-pointer"
+              onClick={() => console.log("participer")}
+              animate={{scale: [1, 1.03, 1]}}
+              transition={{
+                duration: 4,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "easeInOut",
+              }}
+          >
+            <Image
+                src="/images/participer.png"
+                alt="Participer à une chasse"
+                width={435}
+                height={100}
+                priority
+            />
+          </motion.button>
+        </div>
+
+
+        <motion.div initial={{opacity: 0}} animate={canvasControls} className="w-full h-full pointer-events-none">
+          <Canvas gl={{powerPreference: "high-performance"}} camera={{position: [0, 0, 5], fov: 40}}>
+            <ambientLight intensity={0.4}/>
+            <directionalLight position={[50, 50, 15]} intensity={6.5}/>
+            <Earth earthRef={earthRef}/>
+            <OrbitControls enableRotate={false} enableZoom={false} enablePan={false}/>
+            <EffectComposer multisampling={0} enableNormalPass>
+              <BrightnessContrast contrast={-0.15} brightness={-0.01}/>
+              <Autofocus/>
+            </EffectComposer>
+          </Canvas>
+        </motion.div>
+      </div>
   )
 }
