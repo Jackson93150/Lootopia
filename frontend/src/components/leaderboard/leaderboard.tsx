5dc84143c8c77fc4c23a39f90a67829f66ca5722
Jackson93150 <jackjack93150@gmail.com>
2025-05-23 14:38:41 +0200

    add skeleton leaderboard

--- frontend/src/components/leaderboard/leaderboard.tsx
+++ frontend/src/components/leaderboard/leaderboard.tsx
@@ -2,16 +2,20 @@ import type { UserWithId } from "@/app/types/user"
 import { getUsersXp } from "@/service/rewards"
 import { useEffect, useState } from "react"
 import PageContainer from "../container/page-container"
+import SkeletonCell from "../skeleton/cellSkeleton"
 import MonthlyCountdownTimer from "../timer/contdown"
 import Cell from "./cell"
 
 export default function Leaderboard() {
   const [user, setUser] = useState<UserWithId[]>()
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     async function fetchLeaderboard() {
+      setLoading(true)
       const data = await getUsersXp()
       setUser(data)
+      setLoading(false)
     }
 
     fetchLeaderboard()
@@ -29,12 +33,24 @@ export default function Leaderboard() {
             </div>
           </div>
 
-          <div className="w-[95%] flex flex-col lg:flex-row items-center">
+          <div className="w-[98%] flex flex-col lg:flex-row items-center">
             <div className="flex flex-col w-full min-h-0 max-h-[80vh]">
               <div className="w-full flex flex-col p-2 mt-4 items-center gap-4 flex-grow overflow-y-auto">
-                {user?.map((user, index) => (
-                  <Cell key={`cell1-${user.id_user}-${index}`} user={user} index={index} />
-                ))}
+                {loading ? (
+                  <>
+                    <SkeletonCell index={0} />
+                    <SkeletonCell index={1} />
+                    <SkeletonCell index={2} />
+                    <SkeletonCell index={3} />
+                    <SkeletonCell index={3} />
+                    <SkeletonCell index={3} />
+                    <SkeletonCell index={3} />
+                    <SkeletonCell index={3} />
+                    <SkeletonCell index={3} />
+                  </>
+                ) : (
+                  user?.map((user, index) => <Cell key={`cell1-${user.id_user}-${index}`} user={user} index={index} />)
+                )}
               </div>
             </div>
           </div>
